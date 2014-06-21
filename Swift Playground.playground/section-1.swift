ae8e2b000de4b8346e32a4328ca9cd9f9ad0a823
Brad Cerasani <bradcerasani@gmail.com>
2014-06-20 21:50:39 -0500

    for-in loop

--- Swift Playground.playground/section-1.swift
+++ Swift Playground.playground/section-1.swift
@@ -108,7 +108,22 @@ switch vegetable {
 }
 
 // use for-in to iterate over items in a dictionary
-let interesing
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+largest
+
+
 
 
 
